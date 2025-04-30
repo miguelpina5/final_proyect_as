5c3d6d9bff2d12e2f6427e82e30a56f43bebff4a
miguelpina5 <miguelpm2005@gmail.com>
2025-04-30 16:59:51 +0200

    cmake arreglado

--- src/main_bt.cpp
+++ src/main_bt.cpp
@@ -1,6 +1,10 @@
 #include <rclcpp/rclcpp.hpp>
-#include <behaviortree_cpp_v3/behavior_tree.h>
-#include <behaviortree_cpp_v3/xml_parsing.h>
+#include "behaviortree_cpp_v3/behavior_tree.h"
+#include "behaviortree_cpp_v3/bt_factory.h"
+#include "behaviortree_cpp_v3/utils/shared_library.h"
+
+#include "ament_index_cpp/get_package_share_directory.hpp"
+
 #include "final_proyect_as/DetectPersonNode.hpp"
 #include "final_proyect_as/FinishNode.hpp"
 #include "final_proyect_as/RequestPlayersNode.hpp"
@@ -17,9 +21,11 @@ int main(int argc, char ** argv)
     factory.registerFromPlugin(loader.getOSName("detectPerson_bt_node"));
     factory.registerFromPlugin(loader.getOSName("finish_bt_node"));
     factory.registerFromPlugin(loader.getOSName("requestPlayer_bt_node"));
+    factory.registerFromPlugin(loader.getOSName("move_bt_node"));
+    factory.registerFromPlugin(loader.getOSName("getwp_bt_node"));
 
-    std::string pkgpath = ament_index_cpp::get_package_share_directory("bt_nav");
-    std::string xml_file = pkgpath + "/behavior_tree_xml/navigate.xml";
+    std::string pkgpath = ament_index_cpp::get_package_share_directory("final_proyect_as");
+    std::string xml_file = pkgpath + "/BTs/juego_sin_hablar.xml";
 
     auto blackboard = BT::Blackboard::create();
     blackboard->set("node", node);
