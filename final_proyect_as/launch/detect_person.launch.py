dbfbed5e47b345e6f6e29860fc1bd765717e0286
miguelpina5 <miguelpm2005@gmail.com>
2025-05-07 17:09:16 +0200

    fubciona mas o mns

--- final_proyect_as/launch/detect_person.launch.py
+++ final_proyect_as/launch/detect_person.launch.py
@@ -1,31 +1,22 @@
-# Copyright 2024 Intelligent Robotics Lab
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 import os
 
 from ament_index_python.packages import get_package_share_directory
 
 from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 
 
 def generate_launch_description():
-    pkg_dir = get_package_share_directory('camera')
-    param_file = os.path.join(pkg_dir, 'config', 'params.yaml')
+    # Ruta al paquete 'camera' y su archivo de parámetros
+    pkg_camera_dir = get_package_share_directory('camera')
+    param_file = os.path.join(pkg_camera_dir, 'config', 'params.yaml')
 
-    pkg_yolo_dir = get_package_share_directory('yolo_bringup')
-    yolo_launch_file = os.path.join(pkg_yolo_dir, 'launch', 'yolo.launch.py')
+    # Incluir el otro launcher desde el paquete 'yolo_bringup'
+    yolo_bringup_dir = get_package_share_directory('yolo_bringup')
+    yolo_launch_file = os.path.join(yolo_bringup_dir, 'launch', 'yolo.launch.py')
 
     yolo_launcher = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(yolo_launch_file),
@@ -41,24 +32,21 @@ def generate_launch_description():
         }.items()
     )
 
-    detector_yolo = Node(package='camera',
-                        executable='yolo_detection',
-                        output='screen',
-                        parameters=[param_file],
-                        remappings=[
-                          ('input_detection', '/yolo/detections'),
-                          ('output_detection_2d', '/detection_2d')
-                        ])
+    # Nodo de detección de cámara (puedes quitar esto si yolo ya se encarga)
+    detector_yolo = Node(
+        package='camera',
+        executable='yolo_detection',
+        output='screen',
+        parameters=[param_file],
+        remappings=[
+            ('input_detection', '/yolo/detections'),
+            ('output_detection_2d', '/detection_2d')
+        ]
+    )
 
-    final_proyect_as = Node(
-                    package='final_proyect_as',
-                    executable='final_proyect',
-                    output='screen'
-                    )
-
+    # Descripción del lanzamiento
     ld = LaunchDescription()
-    ld.add_action(final_proyect_as)
-    ld.add_action(detector_yolo)
     ld.add_action(yolo_launcher)
+    ld.add_action(detector_yolo)
 
     return ld
